3ffa6b6d7da83f184adbe9b5f6891050502255d7
Rohan Singh <rohan.b.singh54@gmail.com>
2023-05-12 16:56:09 -0400

    Addd Pytoech code

---
+++ PyTorch Fine-tuning/hugging.py
@@ -0,0 +1,25 @@
+"""
+Author: Rohan Singh
+Using the Hugging Face API for fine-tuning
+"""
+
+
+"""
+Key Objects from Hugging Face API:
+1) Dataset:- Splits the data into trainging/testing sets
+2) DataCollator:- Forms batches fo data from the dataset
+3) Traingin Arguments:- Keeps track of trainging arguments liek saving strategy and learning rate scheduler parameters
+4) Trainer:- API funtion for the PyTorch training loop (computing loss -> getting gradiet -> updating weights)
+"""
+
+
+#  Imports
+
+
+
+#  Main function for testing
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
